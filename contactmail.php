789bc1345fe0b9b4254aa98a8bda1660f01ad5f8
abhi07799 <kumawatabhi7799@gmail.com>
2020-07-18 11:48:30 +0530

    gffgfhfjhkh

---
+++ contactmail.php
@@ -0,0 +1,64 @@
+<?php
+
+
+if(isset($_POST["submit"]))
+{
+	$fname=$_POST["firstname"];
+	$lname=$_POST["lastname"];
+	$country=$_POST["country"];
+	$email=$_POST["txtEmail"];
+	$phone=$_POST["txtMobile"];
+	$profile=$_POST["profile"];
+	$company=$_POST["company"];
+	$industry=$_POST["industry"];
+	$interest=$_POST["interest"];
+	$msg=$_POST["message"];
+	
+	$from = "Volunteer Contact";
+	$to="@gmail.com";
+	$webmaster = "info@webnodites.com";
+	$sub = "";
+
+	$header = "From:".$from."<".$webmaster.">\n";
+	$message = "FirstName :".$fname."\n";
+	$message = "LastName :".$lname."\n";
+	$message = "Country :".$country."\n";
+	$message .= "Email :".$email."\n";
+	$message .= "Mobile No. :".$phone."\n";
+	$message .= "Profile :".$profile."\n";
+	$message .= "Company :".$company."\n";
+	$message .= "Industry :".$industry."\n";
+	$message .= "Area Of Interest :".$interest."\n";
+	$message .= "Message :".$msg."\n";
+
+	$sendmail = mail($to,$sub,$message,$header);
+	if($sendmail){
+		echo "<script>alert({
+				  title: 'Thank You!',
+				  text: 'Message Sent Successfully!',
+				  icon: 'success',
+				  button: 'Ok!',
+				}).then(function(){
+			         window.location.href='index';
+			         });
+			   </script>";
+	}
+	else{
+	
+		echo "<script>
+			alert({
+				  title: 'Oops!',
+				  text: 'Something Went Wrong!',
+				  icon: 'error',
+				  button: 'Ok!',
+				}).then(function(){
+			         window.location.href='index.html';
+			         });  
+			</script>";
+
+		}
+
+}
+
+
+?>
